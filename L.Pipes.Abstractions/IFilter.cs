66723d61fe319d85fb1214d5d3db9b7f14f40438
Liveron <minigalimov.lenar@yandex.ru>
2025-08-26 16:12:55 +0300

    version changed

--- L.Pipes.Abstractions/IFilter.cs
+++ L.Pipes.Abstractions/IFilter.cs
@@ -4,3 +4,14 @@ public interface IFilter
 {
     Task HandleAsync(IPipeContext context, FilterDelegate next);
 }
+
+public interface IFilter<TContext> : IFilter where TContext : IPipeContext
+{
+    Task HandleAsync(TContext context, FilterDelegate next);
+    async Task IFilter.HandleAsync(IPipeContext context, FilterDelegate next)
+    {
+        if (context is not TContext typedContext)
+            throw new ArgumentException($"Expected context of type {typeof(TContext).FullName}, but received {context.GetType().FullName}.");
+        await HandleAsync(typedContext, next);
+    }
+}
